c2a799667cd569c492524735364344dbf395b205
Alan Nguyen <nguyentri.alan@gmail.com>
2023-12-27 20:57:59 +0700

    conv sub

--- src/utils/convolution.py
+++ src/utils/convolution.py
@@ -32,22 +32,29 @@ class ConvolutionModule(nn.Module):
 class ConvolutionSubsampling(nn.Module):
     def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
-        self.conv_1 = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv_1 = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=2, padding=1)
         self.act_1 = nn.GELU()
-        self.max_pool_1 = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
         self.conv_2 = nn.Conv1d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
         self.act_2 = nn.GELU()
-        self.max_pool_2 = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
     
     def forward(self, x: torch.Tensor, lengths: Optional[torch.Tensor] = None) -> torch.Tensor:
         x = self.conv_1(x)
         x = self.act_1(x)
-        x = self.max_pool_1(x)
         x = self.conv_2(x)
         x = self.act_2(x)
-        x = self.max_pool_2(x)
 
         if lengths is not None:
-            lengths = torch.ceil(torch.ceil(lengths/2)/4)
+            lengths = torch.ceil(lengths/2).type(torch.int)
             
         return x, lengths
+    
+class SeperableConvolution(nn.Module):
+    def __init__(self, d_model: int, hidden_dim: int) -> None:
+        super().__init__()
+        self.depthwise_conv = nn.Conv1d(in_channels=d_model, out_channels=hidden_dim, kernel_size=3, stride=1, padding=1, groups=d_model)
+        self.pointwise_conv = nn.Conv1d(in_channels=hidden_dim, out_channels=d_model, kernel_size=1, stride=1)
+
+    def forward(self, x :torch.Tensor):
+        x = self.depthwise_conv(x)
+        x = self.pointwise_conv(x)
+        return x

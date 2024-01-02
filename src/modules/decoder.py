ee1ad9e8b4b5c9d258e78f1353b54d75961563d8
Alan-404 <nguyentri.alan@gmail.com>
2024-01-02 09:06:25 +0700

    fix decder

--- src/modules/decoder.py
+++ src/modules/decoder.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
+from typing import Optional
 
 class Decoder(nn.Module):
     def __init__(self, vocab_size: int, d_model: int) -> None:
         super().__init__()
         self.linear = nn.Linear(in_features=d_model, out_features=vocab_size)
 
-    def forward(self, x: torch.Tensor, lengths: Optional[torch.Tensor] = None) -> torch.Tensor:
-        self.linear = nn.Linear(in_features=d_model, out_features=vocab_size)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.linear(x)
+        return x

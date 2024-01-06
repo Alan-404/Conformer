d35d93c32ba468ead85b5b4885b2aa82c51c7e99
Alan Nguyen <nguyentri.alan@gmail.com>
2024-01-06 20:44:57 +0700

    edit

--- src/conformer.py
+++ src/conformer.py
@@ -11,11 +11,11 @@ class Conformer(nn.Module):
                  encoder_n_layers: int, 
                  encoder_dim: int, 
                  heads: int, 
-                 kernel_size: int, 
-                 eps: float=1e-5, 
+                 kernel_size: int,
                  decoder_n_layers: int = 1,
-                 decoder_dim: int = 640,
-                 dropout_rate: float = 0.0) -> None:
+                 decoder_dim: int = 640, 
+                 eps: float=1e-5,
+                 dropout_rate: float = 0.1) -> None:
         super().__init__()
         self.encoder = Encoder(n_mel_channels=n_mel_channels, n=encoder_n_layers, d_model=encoder_dim, heads=heads, kernel_size=kernel_size, eps=eps, dropout_rate=dropout_rate)
         self.decoder = Decoder(vocab_size=vocab_size, d_model=encoder_dim, n=decoder_n_layers, hidden_dim=decoder_dim, eps=eps)

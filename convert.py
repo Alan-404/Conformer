9f832d1ae2d5cd68a40a43a00a4f91c8cbf2d995
Alan-404 <nguyentri.alan@gmail.com>
2024-01-19 12:39:25 +0700

    convert

---
+++ convert.py
@@ -0,0 +1,35 @@
+from glob import glob
+from pydub import AudioSegment
+import os
+from tqdm import tqdm
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument("--folder_path", type=str)
+parser.add_argument("--remove", type=bool, default=False)
+
+args = parser.parse_args()
+
+def convert(path: str):
+    assert ".mp3" in path
+    try:
+        dst = path.replace(".mp3", ".wav")
+
+        sound = AudioSegment.from_mp3(path, format='mp3')
+        sound.export(dst, format="wav")
+    except Exception as e:
+        print(str(e))
+
+files = glob(f"{args.folder_path}/*.mp3")
+
+print("Converting All MP3 Files to Wave Format")
+for item in tqdm(files):
+    convert(item)
+
+if args.remove:
+    print("Deleting All MP3 Files")
+    for item in tqdm(files):
+        os.remove(item)
+
+print("Finish")

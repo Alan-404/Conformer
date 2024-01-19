8b240ed16a1b45ce7fb58a9551443ff39c5e0c36
Alan Nguyen <nguyentri.alan@gmail.com>
2024-01-20 00:00:05 +0700

    wandb

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from ignite.metrics import RunningAverage
 from ignite.handlers import Checkpoint, DiskSaver, EarlyStopping, global_step_from_engine
 from ignite.contrib.handlers.tqdm_logger import ProgressBar
 
+import fire
+
 import torchsummary
 
 from preprocessing.processor import ConformerProcessor
@@ -19,286 +21,192 @@ from src.loss import ctc_loss
 from src.metric import WER_score
 
 from typing import Tuple
+
 import wandb
 
-########################################
-from argparse import ArgumentParser
+wandb.init(project='conformer', name='trind18')
 
-parser = ArgumentParser()
+def train(
+        # Processor Config
+        rank: int = 0,
+        vocab_path: str = None, 
+        train_path: str = None,
+        pad_token: str = "<pad>", 
+        unk_token: str = "<unk>", 
+        word_delim_token: str = "|", 
+        num_mels: int = 80, 
+        sampling_rate: int = 16000, 
+        fft_size: int = 400, 
+        hop_length: int = 160, 
+        win_length: int = 400, 
+        fmin: float = 0.0, 
+        fmax: float = 8000.0,
+        # Model Hyper - Params
+        encoder_n_layers: int = 17,
+        encoder_dim: int = 512,
+        heads: int = 8,
+        kernel_size: int = 31,
+        decoder_n_layers: int = 1,
+        decoder_dim: int = 640,
+        dropout_rate: float = 0.1,
+        # Optimizer
+        lr: float = 1e-4,
+        # Train config
+        checkpoint: str = None,
+        num_train: int = None,
+        batch_size: int = 1,
+        num_epochs: int = 1,
+        saved_checkpoint: str = './checkpoints',
+        early_stopping_patience: int = 2,
+        set_lr: bool = False
+    ):
 
-# Audio Processor Config
-parser.add_argument("--num_mels", type=int, default=80)
-parser.add_argument("--sampling_rate", type=int, default=16000)
-parser.add_argument("--fft_size", type=int, default=400)
-parser.add_argument("--hop_length", type=int, default=160)
-parser.add_argument("--win_length", type=int, default=400)
-parser.add_argument("--fmin", type=float, default=0.0)
-parser.add_argument("--fmax", type=float, default=8000.0)
+    assert vocab_path is not None and train_path is not None and os.path.exists(vocab_path) and os.path.exists(train_path)
 
-# Text Processor Config
-parser.add_argument("--vocab_path", type=str, default="./vocab.json")
-parser.add_argument("--pad_token", type=str, default="<pad>")
-parser.add_argument("--unk_token", type=str, default="<unk>")
-parser.add_argument("--word_delim_token", type=str, default="|")
-
-# Model Config
-parser.add_argument("--encoder_n_layers", type=int, default=17)
-parser.add_argument("--encoder_dim", type=int, default=512)
-parser.add_argument("--heads", type=int, default=8)
-parser.add_argument("--kernel_size", type=int, default=31)
-parser.add_argument("--decoder_n_layers", type=int, default=1)
-parser.add_argument("--decoder_dim", type=int, default=640)
-parser.add_argument("--dropout_rate", type=float, default=0.1)
-
-# Training Config
-parser.add_argument("--num_epochs", type=int, default=1)
-parser.add_argument("--checkpoint", type=str, default=None)
-parser.add_argument("--saved_checkpoint", type=str, default="./checkpoints")
-parser.add_argument("--train_path", type=str, default="./datasets/train.tsv")
-parser.add_argument("--batch_size", type=int, default=1)
-parser.add_argument("--num_train", type=int, default=None)
-
-# Validation Config
-parser.add_argument("--use_validation", type=bool, default=False)
-parser.add_argument("--val_path", type=str, default=None)
-parser.add_argument("--val_size", type=float, default=0.1)
-parser.add_argument("--val_batch_size", type=int, default=1)
-parser.add_argument("--num_val", type=int, default=None)
-
-# Optimizer Config
-parser.add_argument("--set_lr", type=bool, default=False)
-parser.add_argument("--lr", type=float, default=3e-5)
-
-# Early Stopping Config
-parser.add_argument("--early_stopping_patience", type=int, default=3)
-
-# # WanDB Config
-parser.add_argument("--wandb_project_name", type=str, default="(STT) Conformer")
-parser.add_argument("--wandb_username", type=str, default="tri")
-
-########################################
-# Parse Config
-args = parser.parse_args()
-
-wandb.init(project=args.wandb_project_name, name=args.wandb_username)
-
-# Device Config
-device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
-
-scaler = GradScaler()
-
-# Processor Setup
-processor = ConformerProcessor(
-    vocab_path=args.vocab_path,
-    num_mels=args.num_mels,
-    sampling_rate=args.sampling_rate,
-    n_fft=args.fft_size,
-    hop_length=args.hop_length,
-    win_length=args.win_length,
-    fmin=args.fmin,
-    fmax=args.fmax,
-    pad_token=args.pad_token,
-    unk_token=args.unk_token,
-    word_delim_token=args.word_delim_token
-)
-
-# Model Setup
-model = Conformer(
-    vocab_size=len(processor.dictionary.get_itos()),
-    n_mel_channels=processor.num_mels,
-    encoder_n_layers=args.encoder_n_layers,
-    encoder_dim=args.encoder_dim,
-    heads=args.heads,
-    kernel_size=args.kernel_size,
-    decoder_n_layers=args.decoder_n_layers,
-    decoder_dim=args.decoder_dim,
-    dropout_rate=args.dropout_rate
-).to(device)
-
-# Optimizer Setup
-optimizer = optim.Adam(params=model.parameters(), lr=args.lr, weight_decay=1e-6, betas=[0.9, 0.98], eps=1e-9)
-scheduler = lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=1000)
-
-# Dataset and DataLoader Setup
-
-def get_batch(batch, augment: bool) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-    signals, transcripts = zip(*batch)
-    mels, mel_lengths = processor(signals, return_length=True, set_augment=augment)
-    tokens, token_lengths = processor.tokenize(transcripts)
-
-    return mels, tokens, mel_lengths, token_lengths
-
-train_dataset = ConformerDataset(manifest_path=args.train_path, processor=processor, num_examples=args.num_train)
-
-if args.use_validation:
-    if args.val_path is not None:
-        val_dataset = ConformerDataset(manifest_path=args.val_path, processor=processor, num_examples=args.num_val)
+    if torch.cuda.is_available():
+        device = torch.device(f'cuda:{rank}')
     else:
-        train_dataset, val_dataset = random_split(train_dataset, [1 - args.val_size, args.val_size], generator=torch.Generator().manual_seed(41))
-    val_dataloader = DataLoader(dataset=val_dataset, batch_size=args.val_batch_size, shuffle=True, collate_fn=lambda batch: get_batch(batch, False))
+        device = 'cpu'
 
-train_dataloader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=lambda batch: get_batch(batch, True))
-
-# Train and Validate Processing Setup
-def train_step(engine: Engine, batch: Tuple[torch.Tensor]) -> float:
-    inputs = batch[0].to(device)
-    labels = batch[1].to(device)
-
-    input_lengths = batch[2].to(device)
-    target_lengths = batch[3].to(device)
-
-    optimizer.zero_grad()
-
-    with autocast():
-        outputs, input_lengths = model(inputs, input_lengths)
-        loss = ctc_loss(
-            outputs,
-            labels,
-            input_lengths,
-            target_lengths,
-            blank_id=processor.pad_token,
-            zero_infinity=True
-        )
-        
-    scaler.scale(loss).backward()
-    scaler.unscale_(optimizer)
-    scaler.step(optimizer)
-
-    scaler.update()
-    
-    return loss.item()
-
-def val_step(_: Engine, batch: Tuple[torch.Tensor]) -> Tuple[float, float]:
-    inputs = batch[0].to(device)
-    labels = batch[1].to(device)
-
-    input_lengths = batch[2].to(device)
-    target_lengths = batch[3].to(device)
-
-    with torch.no_grad():
-        outputs = model(inputs, input_lengths)
-
-    loss = ctc_loss(
-        outputs,
-        labels,
-        input_lengths,
-        target_lengths,
-        blank_id=processor.pad_token,
-        zero_infinity=True
+    processor = ConformerProcessor(
+        vocab_path=vocab_path,
+        unk_token=unk_token,
+        pad_token=pad_token,
+        word_delim_token=word_delim_token,
+        sampling_rate=sampling_rate,
+        num_mels=num_mels,
+        n_fft=fft_size,
+        hop_length=hop_length,
+        win_length=win_length,
+        fmin=fmin,
+        fmax=fmax
     )
-
-    hypothesis = processor.decode_batch(outputs)
-    reference = processor.decode_batch(labels, group_token=False)
-
-    score = WER_score(hypothesis, reference)
-
-    return loss, score
-
-# Early Stopping Setup
-def val_early_stopping_condition(engine: Engine) -> float:
-    return 1 - engine.state.metrics['score']
-
-# Setup Trainer
-trainer = Engine(train_step)
-train_loss = RunningAverage(output_transform=lambda x: x)
-train_loss.attach(trainer, 'loss')
-ProgressBar().attach(trainer)
-
-# Setup Validator
-validator = Engine(val_step)
-ProgressBar().attach(validator)
-val_loss = RunningAverage(output_transform=lambda x: x[0])
-val_loss.attach(validator, 'loss')
-val_score = RunningAverage(output_transform=lambda x: x[1])
-val_score.attach(validator, 'score')
-
-if args.use_validation:
-    early_stopping_handler = EarlyStopping(patience=args.early_stopping_patience, score_function=val_early_stopping_condition, trainer=trainer)
-
-# Checkpoint Manager Setup
-to_save = {
-    'model': model,
-    'optimizer': optimizer,
-    'lr_scheduler': scheduler,
-    'scaler': scaler
-}
-
-checkpoint_manager = Checkpoint(to_save=to_save, 
-                                save_handler=DiskSaver(args.saved_checkpoint, create_dir=True, require_empty=False),
-                                n_saved=args.early_stopping_patience + 1,
-                                global_step_transform=global_step_from_engine(trainer))
-
-# Trainer Events
-@trainer.on(Events.STARTED)
-def start_training(engine: Engine) -> None:
-    if args.set_lr:
-        optimizer.param_groups[0]['lr'] = args.lr
-    print("\nModel Summary")
-    torchsummary.summary(model, depth=5)
     
-    print("\n================== Training Information ==================")
-    print(f"\tNumber of Samples: {len(engine.state.dataloader.dataset)}")
-    print(f"\tBatch Size: {engine.state.dataloader.batch_size}")
-    print(f"\tNumber of Batches: {len(engine.state.dataloader)}")
-    print(f"\tCurrent Learning Rate: {optimizer.param_groups[0]['lr']}")
-    print("==========================================================\n")
+    model = Conformer(
+        vocab_size=len(processor.dictionary.get_itos()),
+        n_mel_channels=processor.num_mels,
+        encoder_n_layers=encoder_n_layers,
+        encoder_dim=encoder_dim,
+        heads=heads,
+        kernel_size=kernel_size,
+        decoder_n_layers=decoder_n_layers,
+        decoder_dim=decoder_dim,
+        dropout_rate=dropout_rate
+    ).to(device)
 
-    if args.use_validation:
-        print("================== Validation Information ==================")
-        print(f"\tNumber of Samples: {len(val_dataset)}")
-        print(f"\tBatch Size: {args.val_batch_size}")
-        print(f"\tNumber of Batches: {len(val_dataloader)}")
+    optimizer = optim.Adam(params=model.parameters(), lr=lr, weight_decay=1e-6, betas=[0.9, 0.98], eps=1e-9)
+    scheduler = lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=1000)
+    scaler = GradScaler()
+    
+    def get_batch(batch, augment: bool) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        signals, transcripts = zip(*batch)
+        mels, mel_lengths = processor(signals, return_length=True, set_augment=augment)
+        tokens, token_lengths = processor.tokenize(transcripts)
+
+        return mels, tokens, mel_lengths, token_lengths
+
+    train_dataset = ConformerDataset(manifest_path=train_path, processor=processor, num_examples=num_train)
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda batch: get_batch(batch, True))
+
+    def train_step(_: Engine, batch: Tuple[torch.Tensor]) -> float:
+        inputs = batch[0].to(device)
+        labels = batch[1].to(device)
+
+        input_lengths = batch[2].to(device)
+        target_lengths = batch[3].to(device)
+
+        optimizer.zero_grad()
+
+        with autocast():
+            outputs, input_lengths = model(inputs, input_lengths)
+            loss = ctc_loss(
+                outputs,
+                labels,
+                input_lengths,
+                target_lengths,
+                blank_id=processor.pad_token,
+                zero_infinity=True
+            )
+            
+        scaler.scale(loss).backward()
+        scaler.unscale_(optimizer)
+        scaler.step(optimizer)
+
+        scaler.update()
+        
+        return loss.item()
+    
+    trainer = Engine(train_step)
+    train_loss = RunningAverage(output_transform=lambda x: x)
+    train_loss.attach(trainer, 'loss')
+    ProgressBar().attach(trainer)
+
+    to_save = {
+        'model': model,
+        'optimizer': optimizer,
+        'lr_scheduler': scheduler,
+        'scaler': scaler
+    }
+
+    checkpoint_manager = Checkpoint(to_save=to_save, 
+                                save_handler=DiskSaver(saved_checkpoint, create_dir=True, require_empty=False),
+                                n_saved=early_stopping_patience + 1,
+                                global_step_transform=global_step_from_engine(trainer))
+    
+    @trainer.on(Events.STARTED)
+    def _(engine: Engine) -> None:
+        if set_lr:
+            optimizer.param_groups[0]['lr'] = lr
+        print("\nModel Summary")
+        torchsummary.summary(model, depth=5)
+        
+        print("\n================== Training Information ==================")
+        print(f"\tNumber of Samples: {len(engine.state.dataloader.dataset)}")
+        print(f"\tBatch Size: {engine.state.dataloader.batch_size}")
+        print(f"\tNumber of Batches: {len(engine.state.dataloader)}")
+        print(f"\tCurrent Learning Rate: {optimizer.param_groups[0]['lr']}")
         print("==========================================================\n")
 
-    model.train()
+        # if args.use_validation:
+        #     print("================== Validation Information ==================")
+        #     print(f"\tNumber of Samples: {len(val_dataset)}")
+        #     print(f"\tBatch Size: {args.val_batch_size}")
+        #     print(f"\tNumber of Batches: {len(val_dataloader)}")
+        #     print("==========================================================\n")
 
-@trainer.on(Events.EPOCH_STARTED)
-def start_epoch(engine: Engine) -> None:
-    print(f"========= Epoch {engine.state.epoch} ============")
+        model.train()
 
-@trainer.on(Events.EPOCH_COMPLETED)
-def finish_epoch(engine: Engine) -> None:
-    print(f"Train Loss: {(engine.state.metrics['loss']):.4f}")
-    print(f"Learning Rate: {optimizer.param_groups[0]['lr']}")
-    wandb.log({
-        "train_loss": engine.state.metrics['loss'], 
-        'learning_rate': optimizer.param_groups[0]['lr']
-    }, step=engine.state.epoch)
+    @trainer.on(Events.EPOCH_STARTED)
+    def _(engine: Engine) -> None:
+        print(f"========= Epoch {engine.state.epoch} ============")
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def _(engine: Engine) -> None:
+        print(f"Train Loss: {(engine.state.metrics['loss']):.4f}")
+        print(f"Learning Rate: {optimizer.param_groups[0]['lr']}")
+        wandb.log({
+            "train_loss": engine.state.metrics['loss'], 
+            'learning_rate': optimizer.param_groups[0]['lr']
+        }, step=engine.state.epoch)
+        
+        scheduler.step()
+        train_loss.reset()
+        
+        print(f"========== Done Epoch {engine.state.epoch} =============\n")
     
-    scheduler.step()
-    train_loss.reset()
-    if args.use_validation == True:
-        validator.run(val_dataloader, max_epochs=1)
-    
-    print(f"========== Done Epoch {engine.state.epoch} =============\n")
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, checkpoint_manager)
 
-trainer.add_event_handler(Events.EPOCH_COMPLETED, checkpoint_manager)
+    @trainer.on(Events.COMPLETED)
+    def _(_: Engine):
+        print(f"\nLast Model Checkpoint is saved at {checkpoint_manager.last_checkpoint}\n")
 
-@trainer.on(Events.COMPLETED)
-def finish_training(engine: Engine):
-    print(f"\nLast Model Checkpoint is saved at {checkpoint_manager.last_checkpoint}\n")
+    if checkpoint is not None:
+        assert os.path.exists(checkpoint), f"NOT FOUND CHECKPOINT AT {checkpoint}"
+        Checkpoint.load_objects(to_save, checkpoint=torch.load(checkpoint, map_location=device))
 
-# Validator Events
-@validator.on(Events.EPOCH_COMPLETED)
-def finish_validating(engine: Engine) -> None:
-    print(f"Validation Loss {(engine.state.metrics['loss']):.4f}")
-    print(f"Validation WER Score {(engine.state.metrics['score']):.4f}")
-    wandb.log({
-        'val_loss': engine.state.metrics['loss'], 
-        'val_score': engine.state.metrics['score'],
-        'early_stopping_patience': early_stopping_handler.counter
-    }, step=trainer.state.epoch)
+    trainer.run(train_dataloader, max_epochs=num_epochs)
 
-    val_loss.reset()
-    val_score.reset()
 
-if args.use_validation:
-    validator.add_event_handler(Events.EPOCH_COMPLETED, early_stopping_handler)
 
-# Load Checkpoint
-if args.checkpoint is not None:
-    assert os.path.exists(args.checkpoint), f"NOT FOUND CHECKPOINT AT {args.checkpoint}"
-    Checkpoint.load_objects(to_save, checkpoint=torch.load(args.checkpoint, map_location=device))
-
-# Start Training
-trainer.run(train_dataloader, max_epochs=args.num_epochs)
+if __name__ == '__main__':
+    fire.Fire(train)

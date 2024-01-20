a45b5682e548f9315ab6701495c91bac0c019c75
Alan Nguyen <nguyentri.alan@gmail.com>
2024-01-20 09:07:55 +0700

    add fire

--- test.py
+++ test.py
@@ -1,134 +1,125 @@
 import os
 import torch
+import fire
 from preprocessing.processor import ConformerProcessor
 from src.conformer import Conformer
 from tqdm import tqdm
 import pandas as pd
-from argparse import ArgumentParser
 from src.metric import WER_score
-parser = ArgumentParser()
 
-parser.add_argument("--test_path", type=str)
-parser.add_argument("--num_examples", type=int, default=None)
-parser.add_argument("--checkpoint", type=str)
-parser.add_argument("--arpa_path", type=str)
+def test(result_folder: str,
+         test_path: str,
+         vocab_path: str,
+         arpa_path: str,
+         checkpoint: str,
+         num_mels: int = 80,
+         sampling_rate: int = 16000,
+         fft_size: int = 400,
+         hop_length: int = 160,
+         win_length: int = 400,
+         fmin: float = 0.0,
+         fmax: float = 8000.0,
+         pad_token: str = "<pad>",
+         unk_token: str = "<unk>",
+         word_delim_token: str = "|",
+         encoder_n_layers: int = 17,
+         encoder_dim: int = 512,
+         heads: int = 8,
+         kernel_size: int = 31,
+         decoder_n_layers: int = 1,
+         decoder_dim: int = 640,
+         dropout_rate: float = 0.0,
+         num_examples: int = None,
+         saved_name: str = None):
+    if os.path.exists(result_folder) == False:
+        os.mkdir(result_folder)
 
-# Audio Processor Config
-parser.add_argument("--num_mels", type=int, default=80)
-parser.add_argument("--sampling_rate", type=int, default=16000)
-parser.add_argument("--fft_size", type=int, default=400)
-parser.add_argument("--hop_length", type=int, default=160)
-parser.add_argument("--win_length", type=int, default=400)
-parser.add_argument("--fmin", type=float, default=0.0)
-parser.add_argument("--fmax", type=float, default=8000.0)
+    # Device Config
+    device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 
-# Text Processor Config
-parser.add_argument("--vocab_path", type=str, default="./vocabulary/dictionary.json")
-parser.add_argument("--pad_token", type=str, default="<pad>")
-parser.add_argument("--unk_token", type=str, default="<unk>")
-parser.add_argument("--word_delim_token", type=str, default="|")
+    # Processor Setup
+    processor = ConformerProcessor(
+        vocab_path=vocab_path,
+        num_mels=num_mels,
+        sampling_rate=sampling_rate,
+        n_fft=fft_size,
+        hop_length=hop_length,
+        win_length=win_length,
+        fmin=fmin,
+        fmax=fmax,
+        pad_token=pad_token,
+        unk_token=unk_token,
+        word_delim_token=word_delim_token,
+        lm_path=arpa_path
+    )
 
-# Model Config
-parser.add_argument("--encoder_n_layers", type=int, default=17)
-parser.add_argument("--encoder_dim", type=int, default=512)
-parser.add_argument("--heads", type=int, default=8)
-parser.add_argument("--kernel_size", type=int, default=31)
-parser.add_argument("--decoder_n_layers", type=int, default=1)
-parser.add_argument("--decoder_dim", type=int, default=640)
-parser.add_argument("--dropout_rate", type=float, default=0.1)
+    # Model Setup
+    model = Conformer(
+        vocab_size=len(processor.dictionary),
+        n_mel_channels=processor.num_mels,
+        encoder_n_layers=encoder_n_layers,
+        encoder_dim=encoder_dim,
+        heads=heads,
+        kernel_size=kernel_size,
+        decoder_n_layers=decoder_n_layers,
+        decoder_dim=decoder_dim,
+        dropout_rate=dropout_rate
+    ).to(device)
 
-# Path
-parser.add_argument("--result_folder", type=str, default='./results')
-parser.add_argument("--saved_name", type=str, default=None)
+    model.load_state_dict(torch.load(checkpoint, map_location='cpu')['model'])
+    model.to(device)
+    model.eval()
 
-args = parser.parse_args()
+    df = pd.read_csv(test_path, sep="\t")
+    if num_examples is not None:
+        df = df[:num_examples]
+    df['text'] = df['text'].fillna('')
 
-if os.path.exists(args.result_folder) == False:
-    os.mkdir(args.result_folder)
+    time_segment = True
+    if "start" not in df.columns or "end" not in df.columns:
+        time_segment = False
+        df['start'] = None
+        df['end'] = None
 
-# Device Config
-device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+    use_type = True
+    if "type" not in df.columns:
+        use_type = False
+        df['type'] = None
 
-# Processor Setup
-processor = ConformerProcessor(
-    vocab_path=args.vocab_path,
-    num_mels=args.num_mels,
-    sampling_rate=args.sampling_rate,
-    n_fft=args.fft_size,
-    hop_length=args.hop_length,
-    win_length=args.win_length,
-    fmin=args.fmin,
-    fmax=args.fmax,
-    pad_token=args.pad_token,
-    unk_token=args.unk_token,
-    word_delim_token=args.word_delim_token,
-    lm_path=args.arpa_path
-)
+    labels = df['text'].to_list()
+    preds = []
 
-# Model Setup
-model = Conformer(
-    vocab_size=len(processor.dictionary),
-    n_mel_channels=processor.num_mels,
-    encoder_n_layers=args.encoder_n_layers,
-    encoder_dim=args.encoder_dim,
-    heads=args.heads,
-    kernel_size=args.kernel_size,
-    decoder_n_layers=args.decoder_n_layers,
-    decoder_dim=args.decoder_dim,
-    dropout_rate=args.dropout_rate
-).to(device)
+    print('=============== Start Testing ====================')
+    for _, row in tqdm(df.iterrows(), total=df.shape[0]):
+        path = row['path']
+        start, end, role = row['start'], row['end'], row['type']
+        mel = processor.mel_spectrogram(processor.load_audio(path, start, end, role)).unsqueeze(0).to(device)
+        with torch.no_grad():
+            logits = model(mel)
+        
+        preds.append(processor.decode_beam_search(logits[0].cpu().numpy()))
+    print(f"=============== Finish Testing ====================\n")
 
-model.load_state_dict(torch.load(args.checkpoint, map_location='cpu')['model'])
-model.to(device)
-model.eval()
+    print(f"WER Score: {WER_score(preds, labels)}")
 
-df = pd.read_csv(args.test_path, sep="\t")
-if args.num_examples is not None:
-    df = df[:args.num_examples]
-df['text'] = df['text'].fillna('')
+    if saved_name is not None:
+        saved_filename = saved_name
+    else:
+        test_name = os.path.basename(test_path)
+        saved_filename = f"result_{test_name}"
 
-time_segment = True
-if "start" not in df.columns or "end" not in df.columns:
-    time_segment = False
-    df['start'] = None
-    df['end'] = None
+    result = {
+        'path': df['path'].to_list(),
+    }
+    if time_segment:
+        result['start'] = df['start'].to_list()
+        result['end'] = df['end'].to_list()
+    if use_type:
+        result['type'] = df['type'].to_list()
+    result['text'] = labels
+    result['predict'] = preds
 
-use_type = True
-if "type" not in df.columns:
-    use_type = False
-    df['type'] = None
+    pd.DataFrame(result).to_csv(f"{result_folder}/{saved_filename}", sep="\t", index=False)
 
-labels = df['text'].to_list()
-preds = []
-
-print('=============== Start Testing ====================')
-for index, row in tqdm(df.iterrows(), total=df.shape[0]):
-    path = row['path']
-    start, end, role = row['start'], row['end'], row['type']
-    mel = processor.mel_spectrogram(processor.load_audio(path, start, end, role)).unsqueeze(0).to(device)
-    with torch.no_grad():
-        logits = model(mel)
-    
-    preds.append(processor.decode_beam_search(logits[0].cpu().numpy()))
-print(f"=============== Finish Testing ====================\n")
-
-print(f"WER Score: {WER_score(preds, labels)}")
-
-if args.saved_name is not None:
-    saved_filename = args.saved_name
-else:
-    test_name = os.path.basename(args.test_path)
-    saved_filename = f"result_{test_name}"
-
-result = {
-    'path': df['path'].to_list(),
-}
-if time_segment:
-    result['start'] = df['start'].to_list()
-    result['end'] = df['end'].to_list()
-if use_type:
-    result['type'] = df['type'].to_list()
-result['text'] = labels
-result['predict'] = preds
-
-pd.DataFrame(result).to_csv(f"{args.result_folder}/{saved_filename}", sep="\t", index=False)
+if __name__ == '__main__':
+    fire.Fire(test)

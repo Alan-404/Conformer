fe249aea115bccef0f34743fc7f5e78a050bb094
Alan-404 <nguyentri.alan@gmail.com>
2024-01-22 09:03:01 +0700

    fix

--- dataset.py
+++ dataset.py
@@ -65,3 +65,45 @@ class ConformerDataset(Dataset):
             role = 1
 
         return self.processor.load_audio(audio_path, start=start, end=end, role=role), transcript
+    
+class CharDataset(Dataset):
+    def __init__(self, manifest_path: str, processor: ConformerProcessor, num_examples: Optional[int] = None, make_grapheme: bool = False) -> None:
+        super().__init__()
+        self.prompts = pd.read_csv(manifest_path, sep="\t")
+        self.columns = self.prompts.columns
+
+        self.prompts['text'] = self.prompts['text'].fillna('')
+
+        if num_examples is not None:
+            self.prompts = self.prompts[:num_examples]
+
+        if "start" in self.columns and "end" in self.columns:
+            self.prompts['duration'] = self.prompts['end'] - self.prompts['start']
+            self.prompts = self.prompts[(self.prompts['duration'] >= 0.3) & (self.prompts['duration'] <= 30)]
+        
+        if "type" not in self.columns:
+            self.prompts['type'] = None
+
+        self.processor = processor
+
+    def __len__(self) -> int:
+        return len(self.prompts)
+    
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, str]:
+        index_df = self.prompts.iloc[index]
+
+        audio_path = index_df['path']
+        transcript = index_df['text']
+
+        start = end = None
+        if "start" in self.columns and "end" in self.columns:
+            start = index_df["start"]
+            end = index_df['end']
+            
+        role = None
+        if index_df['type'] == "up":
+            role = 0
+        elif index_df['type'] == "down":
+            role = 1
+
+        return self.processor.load_audio(audio_path, start=start, end=end, role=role), transcript

bb8871eba675031022d42781dc0a0a967e048854
Alan-404 <nguyentri.alan@gmail.com>
2024-01-23 11:40:40 +0700

    dynamic shape

--- src/utils/position.py
+++ src/utils/position.py
@@ -28,8 +28,33 @@ class PositionalEncoding(nn.Module):
 
         pos_angles = pos_angles.unsqueeze(0)
         return pos_angles
-    
+
+
+
 class RelativePositionalEncoding(nn.Module):
+    def __init__(self, d_model: int = 512) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.register_buffer('div_term', torch.exp(torch.arange(0, self.d_model, 2, dtype=torch.float32) * -(math.log(10000.0) / d_model))) 
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        pe_positive = torch.zeros(x.size(1), self.d_model)
+        pe_negative = torch.zeros(x.size(1), self.d_model)
+        
+        position = torch.arange(0, x.size(1), dtype=torch.float32).unsqueeze(1)
+
+        pe_positive[:, 0::2] = torch.sin(position * self.div_term)
+        pe_positive[:, 1::2] = torch.cos(position * self.div_term)
+        pe_negative[:, 0::2] = torch.sin(-1 * position * self.div_term)
+        pe_negative[:, 1::2] = torch.cos(-1 * position * self.div_term)
+
+        pe_positive = torch.flip(pe_positive, [0]).unsqueeze(0)
+        pe_negative = pe_negative[1:].unsqueeze(0)
+        pe = torch.cat([pe_positive, pe_negative], dim=1)
+        
+        return pe
+
+class RelPositionalEncoding(nn.Module):
     def __init__(self, d_model: int = 512, max_len: int = 5000) -> None:
         super().__init__()
         self.d_model = d_model

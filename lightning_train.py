a4df6bba2a90993cc93969b846e43b4aa82a75b9
Alan-404 <nguyentri.alan@gmail.com>
2024-01-25 16:17:34 +0700

    ddp

--- lightning_train.py
+++ lightning_train.py
@@ -115,9 +115,9 @@ def train(
     num_gpus = torch.cuda.device_count()
     num_epochs += module.current_epoch
     if num_gpus > 1:
-        trainer = L.Trainer(num_nodes=num_gpus, max_epochs=num_epochs, callbacks=callbacks, strategy='ddp', precision=16)
+        trainer = L.Trainer(devices=num_gpus, max_epochs=num_epochs, callbacks=callbacks, strategy='ddp', precision=16)
     else:
-        trainer = L.Trainer(num_nodes=1, max_epochs=num_epochs, callbacks=callbacks, precision=16)
+        trainer = L.Trainer(devices=1, max_epochs=num_epochs, callbacks=callbacks, precision=16)
 
     dataset = ConformerDataset(train_path, processor=processor, num_examples=num_train)
     

a61d3a5ab0ebc104ba3e61acb13f56301906f957
Alan-404 <nguyentri.alan@gmail.com>
2024-01-26 15:02:11 +0700

    .

--- common.py
+++ common.py
@@ -1,4 +1,4 @@
 from collections import OrderedDict
 
 def map_weights(checkpoint: OrderedDict):
-    checkpoint = OrderedDict((key.replace("model.", ""), value) for key, value in checkpoint['state_dict'].items())
+    checkpoint = OrderedDict((key.replace("model.", ""), value) for key, value in checkpoint)

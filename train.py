e312c3ed21e4e930506756ec1782d49392600ff7
Alan-Nguyen <nguyentri.alan@gmail.com>
2024-02-28 17:48:34 +0700

    fix

--- train.py
+++ train.py
@@ -54,8 +54,6 @@ def train(
         time_augment: int = 10,
         time_mask_ratio: float = 0.05,
         # Validation Config
-        use_validation: bool = False,
-        val_size: Union[float, int] = 0.1,
         val_path: Optional[str] = None,
         num_val: Optional[int] = None,
         val_batch_size: Optional[int] = None,
@@ -111,25 +109,18 @@ def train(
     callbacks = []
     callbacks.append(ModelCheckpoint(dirpath=saved_checkpoint, filename="{epoch}", save_on_train_epoch_end=True, save_last=True))
 
-    if use_validation:
+    if val_path is not None:
         callbacks.append(EarlyStopping(monitor='val_score', verbose=True, mode='min', patience=early_stopping_patience))
 
     dataset = ConformerDataset(train_path, processor=processor, num_examples=num_train)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda batch: get_batch(batch, set_augment), num_workers=num_workers)
     
-    if use_validation:
+    val_dataloader = None
+    if val_path is not None:
+        val_dataset = ConformerDataset(val_path, processor=processor, num_examples=num_val)
         if val_batch_size is None:
             val_batch_size = batch_size
-        if val_path is not None:
-            val_dataset = ConformerDataset(val_path, processor=processor, num_examples=num_val)
-        else:
-            if type(val_size) == int:
-                data_lengths = [dataset.__len__() - val_size, val_size]
-            else:
-                data_lengths = [1 - val_size, val_size]
-            dataset, val_dataset = random_split(dataset, lengths=data_lengths, generator=torch.Generator().manual_seed(41))
-        val_dataloader = DataLoader(val_dataset, batch_size=val_batch_size, shuffle=True, collate_fn=lambda batch: get_batch(batch, False), num_workers=num_workers)
-    
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda batch: get_batch(batch, set_augment), num_workers=num_workers)
+        val_dataloader = DataLoader(dataset=val_dataset, batch_size=val_batch_size, shuffle=True)
 
     if device == 'cpu' or not torch.cuda.is_available():
         strategy = SingleDeviceStrategy(device='cpu')
@@ -141,7 +132,7 @@ def train(
 
     trainer = Trainer(max_epochs=num_epochs, callbacks=callbacks, precision='16-mixed', strategy=strategy)
     
-    trainer.fit(module, train_dataloaders=dataloader, val_dataloaders=val_dataloader if use_validation else None, ckpt_path=checkpoint)
+    trainer.fit(module, train_dataloaders=dataloader, val_dataloaders=val_dataloader, ckpt_path=checkpoint)
 
 if __name__ == '__main__':
     fire.Fire(train)

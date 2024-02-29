2aacde3c2378435f7c27ef2376a46b8ea140eb91
Alan-Nguyen <nguyentri.alan@gmail.com>
2024-02-29 20:17:26 +0700

    fix file

--- libri/libri_light.py
+++ libri/libri_light.py
@@ -8,7 +8,7 @@ import fire
 def convert(path: str, source_dir: str = "."):
     folders = os.listdir(path)
 
-    paths = dict()
+    samples = dict()
     for folder in tqdm(folders):
         items = os.listdir(f'{path}/{folder}')
         for item in items:
@@ -19,7 +19,7 @@ def convert(path: str, source_dir: str = "."):
             for json_item in files:
                 data = json.load(open(json_item, 'r'))
                 filename = os.path.basename(json_item)
-                paths[filename.replace(".json", "")] = {
+                samples[filename.replace(".json", "")] = {
                     "audio": json_item.replace(".json", ".flac"),
                     "segments": data['voice_activity']
                 }
@@ -27,7 +27,7 @@ def convert(path: str, source_dir: str = "."):
     root_folder = path.split("/")[-1]
 
     with open(f"{source_dir}/{root_folder}.json", 'w') as file:
-        json.dump(file)
+        json.dump(samples, file)
 
 if __name__ == '__main__':
     fire.Fire(convert)

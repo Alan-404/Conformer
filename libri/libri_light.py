b2e7bad80ceef3bcbdb5f3abc363e7f231ab0f77
Alan-Nguyen <nguyentri.alan@gmail.com>
2024-02-29 20:14:15 +0700

    data_prepare

---
+++ libri/libri_light.py
@@ -0,0 +1,37 @@
+import os
+import glob
+import json
+from tqdm import tqdm
+
+import fire
+
+def convert(path: str, source_dir: str = "."):
+    folders = os.listdir(path)
+
+    paths = dict()
+    for folder in tqdm(folders):
+        item = os.listdir(f'{path}/{folder}')
+        if len(item) > 1:
+            print("Exeption...")
+            print(item)
+            break
+        item = item[0]
+
+        files = glob.glob(f"{path}/{folder}/{item}/*.json")
+
+        for json_item in files:
+            data = json.load(open(json_item, 'r'))
+            filename = os.path.basename(json_item)
+            paths[filename.replace(".json", "")] = {
+                "audio": json_item.replace(".json", ".flac"),
+                "segments": data['voice_activity']
+            }
+
+    root_folder = path.split("/")[-1]
+
+    with open(f"{source_dir}/{root_folder}.json", 'w') as file:
+        json.dump(file)
+
+if __name__ == '__main__':
+    fire.Fire(convert)
+    

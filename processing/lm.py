2bb80094d5cd1d5831ca3b02c460000cec9cb768
Đức Trí <nguyentri.alan@gmail.com>
2024-09-05 12:30:40 +0700

    .

--- processing/lm.py
+++ processing/lm.py
@@ -135,6 +135,8 @@ class KenCTCDecoder:
             results = self.decoder.decode(emissions_ptr, lengths[batch_idx], self.vocab_size)
             hypos.append(self._to_hypo(results[: self.nbest]))
         
+        if batch_size == 1:
+            return hypos[0]
         return hypos
 
 # class KenLanguageModel:
